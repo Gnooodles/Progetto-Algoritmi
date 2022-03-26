18773ec64af1723baa23d852e45b787232be34c1
Daniele Gnudi <62075147+Gnooodles@users.noreply.github.com>
2022-03-26 16:50:09 +0100

    prova

--- heapSelect.c
+++ heapSelect.c
@@ -40,18 +40,54 @@ int right_child(int i) {
     return 2*i + 2;
 }
 
-void swap(int *x, int *y) {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+void swap(int H[], int x, int y) {
+    int temp = H[x];
+    H[x] = H[y];
+    H[y] = temp;
+}
+
+// moves the item at position i of array a
+// into its appropriate position
+void heapify(int a[], int i, int n){
+    // find left child node
+    int left = left_child(i);
+
+    // find right child node
+    int right = right_child(i);
+
+    // find the largest among 3 nodes
+    int m = i;
+
+    // check if the left node is larger than the current node
+    if (left < n && a[left] < a[m]) {
+        m = left;
+    }
+
+    // check if the right node is larger than the current node
+    if (right < n && a[right] < a[m]) {
+        m = right;
+    }
+
+    // swap the largest node with the current node 
+    // and repeat this process until the current node is larger than 
+    // the right and the left node
+    if (m != i) {
+        swap(a,m,i);
+        heapify(a, m, n);
+    }
+
+}
+
+// converts an array into a heap
+void buildMinHeap(int a[], int n) {
+    int i;
+    for (i = n - 1; i >= 0; i--) {
+        heapify(a, i, n);
+    } 
 }
 
 // insert the item at the appropriate position
 void insert(int a[], int data, int *n) {
-    if (*n >= MAX_LINE_SIZE) {
-        printf("%s\n", "The heap is full. Cannot insert");
-        return;
-    }
     // first insert the time at the last position of the array 
     // and move it up
     a[*n] = data;
@@ -60,57 +96,17 @@ void insert(int a[], int data, int *n) {
 
     // move up until the heap property satisfies
     int i = *n - 1;
-    while (i != 0 && a[parent(i)] < a[i]) {
-        swap(&a[parent(i)], &a[i]);
-        i = parent(i);
+    int p = parent(i);
+    while (i > 0 && a[i] < a[p]) {
+        swap(a,i,p);
+        i = p;
+        p = parent(i);
     }
-}
-
-// moves the item at position i of array a
-// into its appropriate position
-void max_heapify(int a[], int i, int n){
-    // find left child node
-    int left = left_child(i);
-
-    // find right child node
-    int right = right_child(i);
-
-    // find the largest among 3 nodes
-    int largest = i;
-
-    // check if the left node is larger than the current node
-    if (left <= n && a[left] > a[largest]) {
-        largest = left;
-    }
-
-    // check if the right node is larger than the current node
-    if (right <= n && a[right] > a[largest]) {
-        largest = right;
-    }
-
-    // swap the largest node with the current node 
-    // and repeat this process until the current node is larger than 
-    // the right and the left node
-    if (largest != i) {
-        int temp = a[i];
-        a[i] = a[largest];
-        a[largest] = temp;
-        max_heapify(a, largest, n);
-    }
-
-}
-
-// converts an array into a heap
-void buildMaxHeap(int a[], int n) {
-    int i;
-    for (i = n/2; i >= 0; i--) {
-        max_heapify(a, i, n);
-    } 
 }
 
 // deletes the max item and return
-int extract_max(int a[], int *n) {
-    int max_item = a[0];
+int extract_min(int a[], int *n) {
+    int min_item = a[0];
 
     // replace the first item with the last item
     a[0] = a[*n - 1];
@@ -118,45 +114,56 @@ int extract_max(int a[], int *n) {
 
     // maintain the heap property by heapifying the 
     // first item
-    max_heapify(a, 0, *n);
-    return max_item;
+    heapify(a, 0, *n);
+    return min_item;
 }
 
-// prints the heap
-void print_heap(int a[], int n) {
-    int i;
-    for (i = 0; i < n; i++) {
+int getMin(int *H){
+    return H[0];
+}
+
+int heapSelect(int a[], int n, int k){
+
+    buildMinHeap(a, n);                                        //vettore per heap ausiliaria
+    int H2[n];
+
+    H2[0] = a[0];
+
+    for(int i = 1; i < k; i++){
+        int min = extract_min(H2,n);
+
+        int *l = &(a[2*(min-*a)+1]);
+        int *r = &(a[2*(min-*a)+2]);
+        if(l-a < n){
+            insert(H2,*l,&(i)-1);
+        }
+        if(r-a < n){
+            insert(H2,*r,&(i)-1);
+        }
+    }
+    return getMin(&H2);
+}
+
+void printArray(int *a, int n){
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", a[i]);
     }
-    printf("\n");
 }
 
 
 int main(){
-    int a[1000];
+
+    int *a = malloc(1000 * sizeof(int));
 
     int n = scanArray(a); //Leggo da una riga di input un array
 
-    //Costruzione max heap
-    buildMaxHeap(a,n);
+    int k;
+    scanf("%d", &k);
 
-    print_heap(a, n);
-    
-    printf("\n");
+    int res = heapSelect(a,n,k);
 
-    //Estrazione massimo
-    printf("Il massimo è: ");
-    int k = extract_max(a,&n);
-    printf("%d\n",k);
-    print_heap(a, n);
-
-    printf("\n");
-
-    //Inserimento chiave
-    insert(a, 10, &n);
-    print_heap(a,n);
-
-    printf("\n");
+    printf("%d\n", res);
 
     return 0;
 }
